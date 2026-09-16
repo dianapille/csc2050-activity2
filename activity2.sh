adab9ce4858faada7131e6eb2d68b45ffe0608ed
Dayana Pille Villalobos <dpillevi@linux1.csc.calpoly.edu>
2026-09-16 14:40:49 -0700

    Initial commit

--- activity2.sh
+++ activity2.sh
@@ -3,21 +3,30 @@
 # take exactly one command-line argument, which is the name of a C source file
 filename="$1"
 
-# verify the file exists and is a .c file
-if [[ -e "$filename" && "$filename" == *.c ]]; then
-    echo "File exists and is a .c file."
+# verify the file exists
+if [ -e "$filename" ]; then
+	echo "File exists."
 else
-    echo "Error: file does not exist or is not a .c file."
-    exit 1
-fi
+	echo "Error: file does not exist"
+	exit 1
+fi 
+
+# verify if it is a c file
+case "$filename" in
+	*.c)
+		echo "This is a c file"
+		;;
+	*)
+		echo "Error: this is not a c file"
+		exit 1
+		;;
+esac
 
 # extract owner of file
 owner=$(ls -l "$filename" | awk '{print $3}')
-echo "Extracting owner of the file..."
 
 # extract last modified date and time
 last_modified=$(ls -l "$filename" | awk '{print $7, $8, $9}')
-echo "Extracting date and time the file was last modified..."
 
 # create a temporary file
 temp_file=$(mktemp)
